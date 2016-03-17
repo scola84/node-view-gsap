206adcff6e5a7da4b932aa7c781d5c797a7aad12
M.C. van den Boogaart <scola@kvdb.name>
2016-03-17 19:51:49 +0100

    Initial import

---
+++ index.js
@@ -0,0 +1,6 @@
+require('gsap/src/uncompressed/easing/EasePack.js');
+require('gsap/src/uncompressed/plugins/CSSPlugin.js');
+
+module.exports = {
+  TimelineMax: require('gsap/src/uncompressed/TimelineMax.js')
+};
